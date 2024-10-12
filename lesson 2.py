16e9764fd02bcfa7092f312fc445e599dc0a142d
kedir <kediryna@gmail.com>
2024-10-12 19:01:14 +0200

    lesson 2

---
+++ lesson 2.py
@@ -0,0 +1,13 @@
+# ввести 4-х значне число з клавіатури, після чого друкує на екрані стовпчик цифр, з якого це число складається.
+number = 5649
+number1 = number // 1000
+number2 = number % 1000 // 100
+number3 = number % 100 // 10
+number4 = number % 10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+
+
