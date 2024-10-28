c4e39e733b4b137238f526d32f846bff71a8a011
kedir <kediryna@gmail.com>
2024-10-28 23:01:22 +0100

    Create README.md

---
+++ h w 6.py
@@ -0,0 +1,24 @@
+# ##ДЗ 6.2. Конвертер із числа в дату
+# MIN_TIME = 0
+# MAX_TIME = 8640000
+# DAY = (24 * 60 * 60)
+# HOUR = (60 * 60)
+# MINUTE = 60
+# clock = int(input("Enter seconds:"))
+# if clock <= MIN_TIME or clock > MAX_TIME:
+#     print(f"Number within {MIN_TIME} and {MAX_TIME} ")
+#     exit()
+# days = clock // DAY
+# if days >= 0:
+#  print(days,"Days", end= " " )
+# hours = (clock % DAY) // HOUR
+# print(str(hours).zfill(2), end=  ":")
+# minutes = (clock % HOUR) // MINUTE
+# print(str(minutes).zfill(2), end= ":")
+# seconds = clock % MINUTE
+# print(str(seconds).zfill(2), end= " ")
+
+
+
+
+
