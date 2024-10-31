52c408fa93386d771e5f2d0de7b368b4dfe9967e
kedir <kediryna@gmail.com>
2024-10-31 20:06:29 +0100

    Create README.md

---
+++ main.py
@@ -0,0 +1,22 @@
+# Python
+# while True:
+#     try:
+#         calculator = int(input("1. Add\n2. Subtract\n3. Multiply\n4. Divide "))
+#         break
+#     except ValueError:
+#         print("Invalid input. Please enter a number between 1 and 4.")
+#
+# # Add your logic for different operations here, for example:
+# if calculator == 1:
+#     print("You selected addition.")
+# elif calculator == 2:
+#     print("You selected subtraction.")
+# elif calculator == 3:
+#     print("You selected multiplication.")
+# elif calculator == 4:
+#     print("You selected division.")
+# else:
+#     print("Invalid selection.")
+
+
+
