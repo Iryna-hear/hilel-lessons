63606085ee478feeb227015f4f064811d7259a6c
kedir <kediryna@gmail.com>
2024-10-31 19:38:06 +0100

    Create README.md

--- home w 3.1.py
+++ home w 3.1.py
@@ -1,26 +1,62 @@
-operation = int(input("1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter your choice: "))
+# operation = int(input("1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter your choice: "))
+#
+# if operation == 1:
+#     num1 = input("Enter first number: ")
+#     num2 = input("Enter second number: ")
+#     print( "Answer is:" + str(int(num1) + int(num2)))
+# elif operation == 2:
+#     num1 = input("Enter first number: ")
+#     num2 = input("Enter second number: ")
+#     print("Answer is:" + str(int(num1) - int(num2)))
+# elif operation == 3:
+#     num1 = input("Enter first number: ")
+#     num2 = input("Enter second number: ")
+#     print("Answer is:" + str(int(num1) * int(num2)))
+# elif operation == 4:
+#     num1 = input("Enter first number: ")
+#     num2 = input("Enter second number: ")
+#     print("Answer is:" + str(int(num1) / int(num2)))
+#     if num1 or num2 == 0:
+#         print("Cannot divide by zero")
+#
+# else:
+#     print(0)
 
-if operation == 1:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
-    print( "Answer is:" + str(int(num1) + int(num2)))
-elif operation == 2:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
-    print("Answer is:" + str(int(num1) - int(num2)))
-elif operation == 3:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
-    print("Answer is:" + str(int(num1) * int(num2)))
-elif operation == 4:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
-    print("Answer is:" + str(int(num1) / int(num2)))
-    if num1 or num2 == 0:
-        print("Cannot divide by zero")
+#Ваша програма має перенести останній елемент списку з кінця на початок, тобто,
+# останній елемент списку має стати першим. Послідовність інших елементів не має змінюватися.
 
-else:
-    print(0)
+
+
+#
+#  home work 3.3
+#  Ваша програма повинна вміти розділяти один список на два та помістити їх
+# у новий список. Тобто, в результаті повинен вийти список із 2-х списків.
+# list_change = [1, 4, 5, 6]
+# list_half = len(list_change)// 2
+# if len(list_change) % 2 == 0:
+#  half1 = list_change[:list_half]
+#  half2 = list_change[list_half:]
+#  print(half1)
+#  print(half2)
+# else:
+#  half1 = list_change[:list_half]
+#  half2 = list_change[list_half+1:]
+# result = [half1, half2]
+# print(result)
+
+
+# list_change = [1, 4, 5, 6]
+# list_half = len(list_change)// 2
+# if len(list_change) % 2 == 0:
+#  half1 = list_change[:list_half]
+#  half2 = list_change[list_half:]
+#  print(half1)
+#  print(half2)
+# else:
+#  half1 = list_change[:list_half]
+#  half2 = list_change[list_half+1:]
+# result = [half1, half2]
+# print(result)
 
 
 
